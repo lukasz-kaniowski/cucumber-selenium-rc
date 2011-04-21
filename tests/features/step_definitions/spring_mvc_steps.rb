84a5295542fd7b50678fd56f7a71adf8e4424c10
Lukasz Kaniowski <lukasz.kaniowski@gmail.com>
2011-04-21 02:05:48 +0200

    adding steps for simple feature

---
+++ tests/features/step_definitions/spring_mvc_steps.rb
@@ -0,0 +1,33 @@
+require "selenium/client"
+require "rspec/expectations"
+
+def init_browser
+  $browser = Selenium::Client::Driver.new \
+          :host => "localhost",
+          :port => 4444,
+          :browser => "*firefox",
+          :url => "http://localhost:8080",
+          :timeout_in_second => 60
+  $browser.start
+end
+
+init_browser
+
+at_exit do
+  $browser.stop
+end
+
+#step definitions
+
+Given /^I am on the "([^\"]*)" page$/ do |page|
+  $browser.open "/mvc-webapp"
+  $browser.click "link=#{page}"
+end
+
+When /^I press "([^\"]*)"$/ do |arg1|
+  $browser.click "link=#{arg1}"
+end
+
+Then /^I should see "([^\"]*)"$/ do |text|
+  ($browser.text?text).should == true
+end
